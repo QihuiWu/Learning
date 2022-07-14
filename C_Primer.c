a98711df80d08d20cd6ae56670fe521a9db46fba
QihuiWu <108409741+QihuiWu@users.noreply.github.com>
2022-07-14 10:37:00 +0800

    Add files via upload

--- C_Primer.c
+++ C_Primer.c
@@ -52,19 +52,60 @@
 //	}
 //	return 0;
 //}
-#include <stdio.h>
-#include <string.h>
-
-int main(void)
-{
-	int i;
-	char arr[20];
-	scanf("%s", arr);
-	int len = strlen(arr) - 1;
-	for (i = len; i >= 0; i--)
-	{
-		printf("%c", arr[i]);
-	}
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main(void)
+//{
+//	int i;
+//	char arr[20];
+//	scanf("%s", arr);
+//	int len = strlen(arr) - 1;
+//	for (i = len; i >= 0; i--)
+//	{
+//		printf("%c", arr[i]);
+//	}
+//
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//float Eight(float a, float b);
+//
+//int main(void)
+//{
+//	float m, n, ret;
+//	printf("Please enter two numbers:");
+//	scanf("%f %f", &m, &n);
+//	ret = Eight(m, n);
+//	printf("%f", ret);
+//
+//	return 0;
+//}
+//
+//float Eight(float a, float b)
+//{
+//	return (a - b) / (a * b);
+//}
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int lower, upper, i;
+//	printf("Enter lower and upper integer limits: ");
+//	scanf("%d %d", &lower, &upper);
+//	while (lower < upper)
+//	{
+//		int sum = 0;
+//		for (i = lower; i <= upper; i++)
+//		{
+//			sum += i *i;
+//		}
+//		printf("The sums of the squares from %d to %d is %d\n", lower*lower, upper*upper, sum);
+//		printf("Enter next set of limits: ");
+//		scanf("%d %d", &lower, &upper);
+//	}
+//	printf("Done\n");
+//
+//	return 0;
+//}
